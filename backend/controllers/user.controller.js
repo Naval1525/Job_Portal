fe2ac5965e71ec0f8e6aac0e72e676f70f554d12
Naval15 <e23cseu0962@bennett.edu.in>
2024-12-10 19:48:47 +0530

    done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import { User } from "../models/user.model.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dataUri from "../utils/datauri.js";
 import cloudinary from "../utils/cloudinary.js";
