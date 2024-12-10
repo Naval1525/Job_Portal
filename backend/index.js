2751541e1239ab355759a26edd6dbba1ba962d7e
Naval15 <e23cseu0962@bennett.edu.in>
2024-12-10 20:28:00 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ console.log(_dirname);
 
 // CORS configuration
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://job-portal-lavu.onrender.com/",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
